10ae804084e6bd0da211833c7e102e8942790587
Алеся <alesya-stupeneva@mail.ru>
2022-03-22 19:21:31 +0300

    useState,useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [catFacts, setCatFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+useEffect(() => {
+  setIsLoading(true);
+  fetch("https://cat-fact.herokuapp.com/facts")
+
+   .then((response) => response.json())
+   .then((data) => {
+     setCatFacts(data);
+     setIsLoading(false);
+   });
+},[]);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
+        <p>{isLoading && 'Loading...'}</p>
+        <ul>
+          {catFacts.map(({ _id: id, text}) => (
+            <li  
+            style={{
+              listStyleType:'none',
+              border: '1px solid green'
+              }} key={id}>{text}</li>
+          ))}
+        </ul>    
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
@@ -42,4 +67,4 @@ function App() {
   )
 }
 
-export default App
+
---
+++ src/components/PrimaryButton.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function PrimaryButton() {
+  const [color, setColor] = useState('green');
+  function clickButton () {
+      return (color === 'green' ? setColor('blue'): setColor('green'));
+    
+  }
+  return(
+    <button type="button" onClick={() => clickButton()} 
+    style={{
+        background: color,
+        width: '100px',
+        margin: '300px',
+    }}>Button</button>
+  );
+}
+
