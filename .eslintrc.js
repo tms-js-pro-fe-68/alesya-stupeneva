d8a88ff6bbe4137fc1c2215fcff8d4a360412de7
Алеся <alesya-stupeneva@mail.ru>
2022-05-20 23:53:45 +0300

    Custom Hook

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "prettier",
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        'react/prop-types': 0,
+        'react/react-in-jsx-scope': 0,
+        'react/jsx-props-no-spreding': 0,
+    }
+}
---
+++ src/components/CustomHook.jsx
@@ -0,0 +1,70 @@
+import { Box } from "@mui/material";
+import { useEffect,useState } from "react"
+
+const api = 'https://cat-fact.herokuapp.com/facts';
+
+function customHook() {
+    const [chackFact,setChackFact] = useState([]);
+    const [isLoading,setIsLoading] = useState(true);
+    const [errors,setError] = useState([]);
+    const [count,setCount] = useState(0);
+    async function getText() {
+        try {
+            const response = await fetch(api);
+            if (response.status === 200) {
+                response.json()
+                .then(data => { setChackFact(arr => [...arr,data]);
+                    
+                })
+                .finally(()=>{
+                    setIsLoading(false)
+                })
+            }
+            else throw new Error(`Response status is ${response.status} ...`)
+        } catch (error) {            
+            setError(array=>[...array,error.message])
+        }
+     }
+    useEffect(() => {
+        setIsLoading(true)
+        getText()
+     }
+     ,[count])
+    return {isLoading,chackFact, errors, setCount }
+}
+
+
+export default function UseMyCustomHook(){
+    const { isLoading,chackFact, errors, setCount } = customHook();
+    return (
+        <Box sx={{
+            display: 'grid',
+            direction: 'column',
+            gap:1,
+            p:4,
+        }}>
+        <p>
+            Errors: {errors}
+        </p>
+        <p>
+            {isLoading && 'Loading, wait ...'}
+        </p>
+        <button 
+            type = 'button'
+            onClick = {()=>{setCount(i => i + 1)}}>
+                Click me, please
+        </button>
+        <div >
+            {chackFact.map((fact) => 
+            <div key={fact.id}
+            style = {{
+                    color: 'red',
+                    p: 4,
+                    }}>
+            {fact.value}
+            </div> 
+             )}
+        </div>
+        </Box>
+       )
+}
